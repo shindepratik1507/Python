e3d2e85ceb6b50f017424cc89e6a11b59a4b6904
Pratik Shinde <168270860+shindepratik1507@users.noreply.github.com>
2025-03-03 11:21:24 +0530

    Add files via upload

---
+++ operations_using_function.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    ans = a+b
+    print("addition is : ",ans)
+def sub(a,b):
+    ans = a-b
+    print("substraction is : ",ans)
+def mult(a,b):
+    ans = a*b
+    print("multiplication is : ",ans)
+def div(a,b):
+    ans = a/b
+    print("division is : ",ans)
+
+num1 = int(input("enter first number : "))
+num2 = int(input("enter second number :"))
+add(num1,num2)
+sub(num1,num2)
+mult(num1,num2)
+div(num1,num2)
