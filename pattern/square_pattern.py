509a20245b0520d4d03de014640881de1c16ded7
Pratik Shinde <168270860+shindepratik1507@users.noreply.github.com>
2025-03-04 11:42:27 +0530

    Add files via upload

---
+++ pattern/square_pattern.py
@@ -0,0 +1,8 @@
+#square pattern
+row=5
+col=5
+
+for i in range(row):
+    for j in range(col):
+        print("*",end=" ")
+    print(" ")
---
+++ pattern/triangle_pattern.py
@@ -0,0 +1,8 @@
+#Triangle Pattern
+row=5
+#col=5
+
+for i in range(row+1):
+    for j in range(i):
+        print("*",end=" ")
+    print(" ")
