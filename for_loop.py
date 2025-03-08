36d6c54e04e86aacfa5f9db393f368f60cb6fe14
Pratik Shinde <168270860+shindepratik1507@users.noreply.github.com>
2025-03-08 13:32:48 +0530

    Create for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,18 @@
+#loops in python
+#for loop
+fruits = ['apple', 'banana', 'cherry']
+for fruit in fruits:
+    print(fruit)
+
+#for loop using range function
+for i in range(5):
+    print(i)
+
+#range(start, stop, step)
+for i in range(1, 10, 2):
+    print(i)
+
+#in the string
+word = "Python"
+for letter in word:
+    print(letter)
