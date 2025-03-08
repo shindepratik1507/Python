8d612573bf24708ecc992523ad05c78b814c2b8e
Pratik Shinde <168270860+shindepratik1507@users.noreply.github.com>
2025-03-08 12:53:08 +0530

    Create Assignment_operator.py

---
+++ Assignment_operator.py
@@ -0,0 +1,25 @@
+#operator in python
+#Assignment operator
+
+x = 10
+
+x += 5   # x = x + 5
+print(x) # Output: 15
+
+x -= 3   # x = x - 3
+print(x) # Output: 12
+
+x *= 2   # x = x * 2
+print(x) # Output: 24
+
+x /= 4   # x = x / 4
+print(x) # Output: 6.0
+
+x %= 5   # x = x % 5
+print(x) # Output: 1.0
+
+x **= 3  # x = x ** 3
+print(x) # Output: 1.0
+
+x //= 2  # x = x // 2
+print(x) # Output: 0.0
